11c1b1441211a8ca6a4b86eb783d2adfbe1c9156
Christian Hansen <cralle@outlook.com>
2016-03-09 13:54:16 +0100

    Added projects

--- site/backend/db.py
+++ site/backend/db.py
@@ -4,7 +4,7 @@ class Member(models.Model):
     last_name = models.CharField(max_length=128)
     first_name = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
-    website = models.CharField(max_length=128)
+    website = models.URLField(max_length=128)
     location = models.CharField(max_length=128) # office
     workplace = models.CharField(max_length=128)
     status = models.CharField(max_length=128)
@@ -16,12 +16,20 @@ class Member(models.Model):
 class Group(models.Model):
     name = models.CharField(max_length=128)
     members = models.ManyToManyField(Person, through='Membership')
+    location = models.CharField(max_leng
 
     def __str__(self):
         return self.name
 
 class Membership(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    person = models.ForeignKey(Member, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateField()
     invite_reason = models.CharField(max_length=64)
+
+class Project(models.Model):
+    owner = models.ForeignKey(Member, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    deadline = models.DateField()
+    subject = models.CharField(max_length=128)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL)
