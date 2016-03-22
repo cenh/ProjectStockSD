3ae1f8135466cfd6e459b90c0322849f4214a381
Lasse HH <ninjalf2@gmail.com>
2016-03-22 11:13:00 +0100

    some additions

--- site/backend/project_stock/projects/models.py
+++ site/backend/project_stock/projects/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 class Supervisor(models.Model):
-    first_name = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
     email = models.CharField(max_length=128)
-    website = models.CharField(max_length=128, blank=True)
+    website = models.URLField(max_length=128, blank=True)
     location = models.CharField(max_length=128, blank=True) # office
     workplace = models.CharField(max_length=128, blank=True)
     status = models.CharField(max_length=128, blank=True)
@@ -17,11 +17,33 @@ class Supervisor(models.Model):
 
 class Project(models.Model):
     supervisor = models.ForeignKey(Supervisor, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=32)
-    subject = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    subject = models.CharField(max_length=128)
     description_short = models.CharField(max_length=256)
     description_long = models.TextField()
-    pub_date = models.DateField('date published')
+    website = models.URLField(max_length=128, blank=True)
+    pub_date = models.DateField(auto_now=True, auto_now_add=True)
+    start_date = models.DateField()
+    deadline = models.DateField()
 
     def __str__(self):
         return self.name
+'''
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+    location = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+class Presentations(models.Model):
+    person = models.ForeignKey(Supervisor, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    date = models.DateField()
+    website = models.URLField(max_length=128)
+
+    def __str__(self):
+        return self.name
+'''
