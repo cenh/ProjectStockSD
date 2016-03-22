f15e1b439d0f86baa491ee40193346a0ec22d1d9
Lasse HH <ninjalf2@gmail.com>
2016-03-22 13:40:36 +0100

    Created project_stock app

---
+++ site/backend/project_stock/project_stock/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProjectStockConfig(AppConfig):
+    name = 'project_stock'
--- site/backend/project_stock/project_stock/views.py
+++ site/backend/project_stock/project_stock/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def example(request):
-    return render(request, 'templates/project_stock/example.html')
+    return render(request, 'project_stock/example.html')
