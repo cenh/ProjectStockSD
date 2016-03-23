b2723334319794198a5e181f18a30cb765be7724
Lasse HH <ninjalf2@gmail.com>
2016-03-23 16:12:24 +0100

    Better field names

--- site/backend/project_stock/projects/models.py
+++ site/backend/project_stock/projects/models.py
@@ -10,8 +10,8 @@ class Supervisor(models.Model):
     workplace = models.CharField(max_length=128, blank=True, null=True)
     status = models.CharField(max_length=128, blank=True, null=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True, null=True) # validators should be a list
-    mobile_number = models.CharField(validators=[phone_regex], max_length=15, blank=True, null=True)
+    phone = models.CharField(validators=[phone_regex], max_length=15, blank=True, null=True) # validators should be a list
+    mobile = models.CharField(validators=[phone_regex], max_length=15, blank=True, null=True)
     fax = models.CharField(validators=[phone_regex], max_length=15, blank=True, null=True)
 
     def __str__(self):
