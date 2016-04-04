0cac19e8ef35f86667d31b67d96d3103d22735e8
cenh <cralle@outlook.com>
2016-04-04 20:36:18 +0100

    Commented it out, work in progress

--- site/backend/project_stock/projects/urls.py
+++ site/backend/project_stock/projects/urls.py
@@ -5,15 +5,15 @@ urlpatterns = [
     # projects
     url(r'^projects/$', views.ProjectView.as_view(), name='project'),
     
-    # /projects/ID
-    url(r'^projects/(?P<project_id>[0-9]+)$', views.ProjectDetailView.as_view() , name='project'),
-
     # supervisors
     url(r'^supervisors/$', views.SupervisorView.as_view(), name='supervisor'),
-
+'''
     # /supervisors/ID
     url(r'^supervisors/(?P<supervisor_id>[0-9]+)$', views.SupervisorDetailView.as_view() , name='supervisor'),
 
+    # /projects/ID
+    url(r'^projects/(?P<project_id>[0-9]+)$', views.ProjectDetailView.as_view() , name='project'),
+'''
     # /projects/ or /supervisors/
     url(r'^$', views.ProjectView.as_view(), name='project'),
 ]
--- site/backend/project_stock/projects/views.py
+++ site/backend/project_stock/projects/views.py
@@ -9,24 +9,25 @@ class ProjectView(generic.ListView):
 
     def get_queryset(self):
         return Project.objects.order_by('id')
-        
-class ProjectDetailView(generic.DetailView, project_id):
-    model = Supervisor
-    template_name = 'projects/profile.html'
 
-    def get_queryset(self):
-        return Project.objects.get(pk=project_id)
-
-class SupervisorView(generic.ListView):
+    class SupervisorView(generic.ListView):
     model = Supervisor
     template_name = 'supervisors/index.html'
 
     def get_queryset(self):
         return Supervisor.objects.order_by('id')
-
+'''
 class SupervisorDetailView(generic.DetailView, supervisor_id):
     model = Supervisor
     template_name = 'supervisors/profile.html'
 
     def get_queryset(self):
         return Supervisor.objects.get(pk=supervisor_id)
+
+class ProjectDetailView(generic.DetailView, project_id):
+    model = Project
+    template_name = 'projects/profile.html'
+
+    def get_queryset(self):
+        return Project.objects.get(pk=project_id)
+'''
