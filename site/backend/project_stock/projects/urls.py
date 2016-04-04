3fcbd6a30fc52daa96da27517abe0dbddc95b3f5
Christian Hansen <cralle@outlook.com>
2016-04-04 19:58:14 +0200

    Ikke sikker, ikke testet

--- site/backend/project_stock/projects/urls.py
+++ site/backend/project_stock/projects/urls.py
@@ -2,11 +2,17 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # /projects/projects
+    # projects
     url(r'^projects/$', views.ProjectView.as_view(), name='project'),
+    
+    # /projects/ID
+    url(r'^projects/(?P<project_id>[0-9]+)$', views.ProjectDetailView.as_view() , name='project'),
 
-    # /projects/supervisors
+    # supervisors
     url(r'^supervisors/$', views.SupervisorView.as_view(), name='supervisor'),
+
+    # /supervisors/ID
+    url(r'^supervisors/(?P<supervisor_id>[0-9]+)$', views.SupervisorDetailView.as_view() , name='supervisor'),
 
     # /projects/ or /supervisors/
     url(r'^$', views.ProjectView.as_view(), name='project'),
