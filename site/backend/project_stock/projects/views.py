f52cdd1b827431bf9173ec21a038beaee8cc093d
Lasse HH <ninjalf2@gmail.com>
2016-04-11 21:31:10 +0200

    Fixed issue #14

--- site/backend/project_stock/projects/views.py
+++ site/backend/project_stock/projects/views.py
@@ -22,6 +22,13 @@ class SupervisorDetailView(generic.DetailView):
     model = Supervisor
     template_name = 'supervisors/profile.html'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(SupervisorDetailView, self).get_context_data(**kwargs)
+        context['project_list'] = Project.objects.all()
+
+        return context
+
 class ProjectDetailView(generic.DetailView):
     model = Project
     template_name = 'projects/profile.html'
