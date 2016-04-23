e8c3860e2efbfb9a618c5583f1e9c2b205d5383d
Lasse HH <ninjalf2@gmail.com>
2016-04-23 04:06:45 +0200

    debug settings

--- site/backend/project_stock/project_stock/settings.py
+++ site/backend/project_stock/project_stock/settings.py
@@ -80,10 +80,8 @@ WSGI_APPLICATION = 'project_stock.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'project_stock/config.cnf'),
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
