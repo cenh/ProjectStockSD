b08132cb16dcdeaee881c674faa9ba8a590f1d13
cenh <cralle@outlook.com>
2016-04-24 13:49:17 +0100

    Frontpage

--- site/backend/project_stock/project_stock/views.py
+++ site/backend/project_stock/project_stock/views.py
@@ -2,13 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project, Supervisor, Group, Publication
 from django.views import generic
+from django.db.models import Count
 
 class IndexView(generic.ListView):
     model = Project
     template_name = 'index.html'
 
     def get_queryset(self):
-        return Project.objects.order_by('pub_date')
+        return Project.objects.order_by('-pub_date')
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(IndexView, self).get_context_data(**kwargs)
+        #context['supervisor_list'] = Supervisor.objects.annotate(num_projects=Count('Project')).order_by('-num_projects')[0:5]
+        context['supervisor_list'] = Supervisor.objects.order_by('last_name')[:5]
+        context['random_project_list'] = Project.objects.order_by('?')[:1]
+        context['project_bachelor'] = Project.objects.filter(type='B').order_by('-pub_date')[:5]
+        context['project_master'] = Project.objects.filter(type='M').order_by('-pub_date')[:5]
+        context['project_thesis'] = Project.objects.filter(type='T').order_by('-pub_date')[:5]
+        return context
 
 class ProjectView(generic.ListView):
     model = Project
@@ -43,7 +55,7 @@ class SupervisorDetailView(generic.DetailView):
         # Call the base implementation first to get a context
         context = super(SupervisorDetailView, self).get_context_data(**kwargs)
         context['project_list'] = Project.objects.order_by('name')
-
+        context['group_list'] = Group.objects.order_by('id')
         return context
 
 class GroupDetailView(generic.DetailView):
@@ -54,6 +66,6 @@ class GroupDetailView(generic.DetailView):
         # Call the base implementation first to get a context
         context = super(GroupDetailView, self).get_context_data(**kwargs)
         context['supervisor_list'] = Supervisor.objects.order_by('last_name')
-
+        context['project_list'] = Project.objects.order_by('name')
         return context
 
