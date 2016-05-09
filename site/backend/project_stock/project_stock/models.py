ed1be5e930c8d8ed0232cb4208d117d68377c492
Lasse HH <ninjalf2@gmail.com>
2016-05-09 09:58:52 +0200

    Added defaults for string fields

--- site/backend/project_stock/project_stock/models.py
+++ site/backend/project_stock/project_stock/models.py
@@ -8,18 +8,18 @@ class Supervisor(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.EmailField()
-    website = models.URLField(max_length=128, blank=True)
-    location = models.CharField(max_length=128, blank=True) # office
-    workplace = models.CharField(max_length=128, blank=True)
-    status = models.CharField(max_length=128, blank=True)
+    website = models.URLField(max_length=128, blank=True, default='')
+    location = models.CharField(max_length=128, blank=True, default='') # office
+    workplace = models.CharField(max_length=128, blank=True, default='')
+    status = models.CharField(max_length=128, blank=True, default='')
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone = models.CharField(validators=[phone_regex], max_length=15, blank=True) # validators should be a list
-    mobile = models.CharField(validators=[phone_regex], max_length=15, blank=True)
-    fax = models.CharField(validators=[phone_regex], max_length=15, blank=True)
+    phone = models.CharField(validators=[phone_regex], max_length=15, blank=True, default='') # validators should be a list
+    mobile = models.CharField(validators=[phone_regex], max_length=15, blank=True, default='')
+    fax = models.CharField(validators=[phone_regex], max_length=15, blank=True, default='')
 
     # this is a base64 field. Easy to scrape and link but how to manage from admin page?
     # 2^25 should be about 32MB in characters
-    photo = models.TextField(max_length=2**25, blank=True)
+    photo = models.TextField(max_length=2**25, blank=True, default='')
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
@@ -36,10 +36,10 @@ class Project(models.Model):
                     ('C', 'Project with compay'),
                     ('O', 'Other'))
     type = models.CharField(max_length=1, choices=type_choices)
-    subject = models.CharField(max_length=128)
-    description_short = models.TextField(max_length=256, blank=True, verbose_name='Short description')
-    description_long = models.TextField(blank=True, verbose_name='Long description')
-    website = models.URLField(max_length=128, blank=True)
+    subject = models.CharField(max_length=128, default='')
+    description_short = models.TextField(max_length=256, blank=True, verbose_name='Short description', default='')
+    description_long = models.TextField(blank=True, verbose_name='Long description', default='')
+    website = models.URLField(max_length=128, blank=True, default='')
     pub_date = models.DateField(auto_now_add=True)
     start_date = models.DateField(blank=True, null=True)
     deadline = models.DateField(blank=True, null=True)
@@ -51,9 +51,9 @@ class Project(models.Model):
 class Publication(models.Model):
     author = models.ForeignKey(Supervisor, on_delete=models.SET_NULL, blank=True, null=True)
     title = models.TextField(max_length=128)
-    description_short = models.TextField(max_length=256, blank=True)
-    description_long = models.TextField(blank=True)
-    website = models.URLField(max_length=128, blank=True)
+    description_short = models.TextField(max_length=256, blank=True, default='')
+    description_long = models.TextField(blank=True, default='')
+    website = models.URLField(max_length=128, blank=True, default='')
     pub_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -63,10 +63,10 @@ class Publication(models.Model):
 class Group(models.Model):
     members = models.ManyToManyField(Supervisor)
     name = models.CharField(max_length=128)
-    subject = models.TextField(max_length=64, blank=True)
-    description = models.TextField(blank=True)
-    website = models.URLField(max_length=128, blank=True)
-    location = models.CharField(max_length=128, blank=True)
+    subject = models.TextField(max_length=64, blank=True, default='')
+    description = models.TextField(blank=True, default='')
+    website = models.URLField(max_length=128, blank=True, default='')
+    location = models.CharField(max_length=128, blank=True, default='')
 
     def __str__(self):
         return self.name
