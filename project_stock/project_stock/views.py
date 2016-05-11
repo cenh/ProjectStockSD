37bf35260240e8a972419f5ffca3a84079907347
cenh <cralle@outlook.com>
2016-05-11 11:49:04 +0100

    Moved status pages to new folder

--- project_stock/project_stock/views.py
+++ project_stock/project_stock/views.py
@@ -7,12 +7,12 @@ from django.utils import timezone
 from django.template import RequestContext
 
 def not_found(request):
-    response = render_to_response('404.html', context_instance=RequestContext(request))
+    response = render_to_response('status/404.html', context_instance=RequestContext(request))
     response.status_code = 404
     return response
 
 def bad_request(request):
-    response = render_to_response('500.html', context_instance=RequestContext(request))
+    response = render_to_response('status/500.html', context_instance=RequestContext(request))
     response.status_code = 500
     return response
 
