9000a75218eeae9b2ab5151ea5d6e176def06354
cenh <cralle@outlook.com>
2016-05-11 11:06:33 +0100

    404 and 500 pages created together with some more css and better look

--- project_stock/project_stock/urls.py
+++ project_stock/project_stock/urls.py
@@ -1,21 +1,9 @@
-"""project_stock URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
+from django.conf.urls import include, url, handler404, handler500
 from django.contrib import admin
 from . import views
+
+handler404 = 'project_stock.views.not_found'
+handler500 = 'project_stock.views.bad_request'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
