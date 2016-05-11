ff7f1bf73524a8ad907e2a214fe4030cc3a8d556
Lasse HH <ninjalf2@gmail.com>
2016-05-11 13:15:18 +0200

    Added admins

--- project_stock/project_stock/settings.py
+++ project_stock/project_stock/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+ADMINS = [('cenh', 'cralle@outlook.com'), ('Lasse', 'ninjalf2@gmail.com')]
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
